a88a3db9a6a030a5535042ffb712eec1f2a5961a
Mayan Bhadage <mbhadag1@binghamton.edu>
2019-12-23 14:09:07 -0500

    Initial Commit

---
+++ Arrays/1.even_odd.py
@@ -0,0 +1,34 @@
+'''
+Reorder entries in an array so that even entries appear first
+without using any additional data structure
+'''
+
+#with additional array
+
+def even_odd(array):
+    even = []
+    odd = []
+    for num in array:
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odd.append(num)
+    even.extend(odd)
+
+    return even
+
+# without using additional data structure
+
+def even_odd_wo(array):
+    start , end = 0 , len(array)-1
+
+    while(start < end):
+        if array[start] % 2 == 0:
+            start += 1
+        else:
+            array[start], array[end] = array[end], array[start]
+            end -= 1
+    return array
+
+
+print(even_odd_wo([3,2,5,6,8,1,7,10,9,4]))
